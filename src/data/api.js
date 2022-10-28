c89d17e764a9305213c71bd80f2b1976779c84dd
GerardoGepda <gerardo.gepda@gmail.com>
2022-10-28 16:16:34 -0600

    modal para editar la info del usuario

--- src/data/api.js
+++ src/data/api.js
@@ -1,4 +1,4 @@
 export default {
-    server:"http://192.168.0.11/backend-app/",
+    server:"http://192.168.100.2/backend-app/",
 
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,9 +2,10 @@ import React,{useEffect,useState} from "react";
 import { ScrollView, StyleSheet, TouchableWithoutFeedback, View , BackHandler} from "react-native";
 import Constants from 'expo-constants';
 import Colors from "../utils/Colors";
-import { Avatar, Card, Text, Button } from "react-native-paper";
+import { Avatar, Card, Text, Button, Portal, Modal, Provider } from "react-native-paper";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import PrimaryInput from './../components/PrimaryInput';
 
 
 export default function Profile() {
@@ -13,6 +14,15 @@ export default function Profile() {
         const [userMail,setMail] = useState();
         const [userDui,setDui] = useState();
         const [userCel,setCel] = useState();
+        //new password
+        const [newPassword, setNewPassword] = useState("");
+
+        //handle if the modal should be visible or not.
+        const [visible, setVisible] = React.useState(false);
+        const showModal = () => setVisible(true);
+        const hideModal = () => setVisible(false);
+        //styles for the modal
+        const containerStyle = {backgroundColor: 'white', padding: 20, paddingBottom: 10, margin: 10};
         
         const Logout = async () =>{
             await AsyncStorage.clear();
@@ -31,66 +41,124 @@ export default function Profile() {
         useEffect( () =>{
             dataUsuario()
         },[])
+
+        //this function reset the information values when when the edition is canceled
+        const editionCanceld = () => {
+            dataUsuario();
+            setNewPassword("");
+            hideModal();
+        }
     
     return (
         //main container, use it for put your code
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Text variant="titleLarge" style={styles.title}>
-                    Mi perfil
-                </Text>
-                <Avatar.Image size={80} source={{uri:'https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg'}} />
-                <Text variant="headlineSmall" style={styles.userName}>{userNameN}</Text>
-            </View>
-            <ScrollView style={styles.mainContent}>
-                <Text variant="titleLarge" style={{color: Colors.primary, fontWeight: 'bold', marginBottom: 10}}>
-                    Mi información
-                </Text>
-                <Card style={styles.cardInfo} mode='elevated'>
-                    <Text variant="titleMedium" style={{color: Colors.primary, fontWeight: 'bold'}}>
-                        Correo electrónico
+        <Provider>
+            <View style={styles.container}>
+                <Portal>
+                    <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
+                        <Text variant="bodyLarge" style={{marginBottom: 20, fontWeight: 'bold'}}>
+                            Editar información de la cuenta
+                        </Text>
+                        <PrimaryInput
+                            viewStyle={{marginBottom: 15}}
+                            title="Editar nombre"
+                            value={userNameN}
+                            onChangeText={setName}
+                            placeholder="Nuevo nombre..."
+                        />
+                        <PrimaryInput
+                            viewStyle={{marginBottom: 15}}
+                            title="Editar correo"
+                            value={userMail}
+                            onChangeText={setMail}
+                            placeholder="Nuevo correo..."
+                            keyboardType="email-address"
+                        />
+                        <PrimaryInput
+                            viewStyle={{marginBottom: 15}}
+                            title="Editar número teléfonico"
+                            value={userCel}
+                            onChangeText={setCel}
+                            placeholder="Nuevo número..."
+                            keyboardType="phone-pad"
+                        />
+                        <PrimaryInput
+                            viewStyle={{marginBottom: 15}}
+                            title="Editar contraseña"
+                            value={newPassword}
+                            onChangeText={setNewPassword}
+                            placeholder="Nueva contraseña..."
+                            secureTextEntry={true}
+                        />
+                        <Button 
+                            textColor={Colors.primary} 
+                            style={{marginTop: 10}}
+                            icon="content-save"
+                            onPress={() => {}} 
+                        >
+                            Guardar
+                        </Button>
+                        <Button textColor="#EA3333" icon="cancel" onPress={editionCanceld}>
+                            Cancelar
+                        </Button>
+                    </Modal>
+                </Portal>
+                <View style={styles.header}>
+                    <Text variant="titleLarge" style={styles.title}>
+                        Mi perfil
                     </Text>
-                    <View style={styles.infoItem}>
-                        <MaterialCommunityIcons name="email" size={24} color={Colors.primary} style={{marginRight: 10}}/>
-                        <Text variant="bodyLarge">{userMail}</Text>
-                    </View>
-                    <Text variant="titleMedium" style={{color: Colors.primary, fontWeight: 'bold'}}>
-                        Número teléfonico
-                    </Text>
-                    <View style={styles.infoItem}>
-                        <MaterialCommunityIcons name="phone" size={24} color={Colors.primary} style={{marginRight: 10}}/>
-                        <Text variant="bodyLarge">{userCel}</Text>
-                    </View>
-                    <Text variant="titleMedium" style={{color: Colors.primary, fontWeight: 'bold'}}>
-                        DUI
-                    </Text>
-                    <View style={styles.infoItem}>
-                        <MaterialCommunityIcons name="clipboard-account" size={24} color={Colors.primary} style={{marginRight: 10}}/>
-                        <Text variant="bodyLarge">{userDui}</Text>
-                    </View>
-                </Card>
-                <View style={styles.options}>
-                    <TouchableWithoutFeedback onPress={() => {}}>
-                        <View style={styles.optionItem}>
-                            <MaterialCommunityIcons name="car" size={24} color={Colors.primary} style={{marginRight: 10, marginLeft: 5}}/>
-                            <Text variant="bodyLarge">Ver mis vehículos</Text>
-                        </View>
-                    </TouchableWithoutFeedback>
-                    <TouchableWithoutFeedback onPress={() => {}}>
-                        <View style={styles.optionItem}>
-                            <MaterialCommunityIcons name="form-textbox-password" size={24} color={Colors.primary} style={{marginRight: 10, marginLeft: 5}}/>
-                            <Text variant="bodyLarge">Cambiar contraseña</Text>
-                        </View>
-                    </TouchableWithoutFeedback>
-                    <TouchableWithoutFeedback onPress={Logout}>
-                        <View style={styles.optionItem}>
-                            <MaterialCommunityIcons name="logout" size={24} color={Colors.primary} style={{marginRight: 10, marginLeft: 5}}/>
-                            <Text variant="bodyLarge">Cerrar sesión</Text>
-                        </View>
-                    </TouchableWithoutFeedback>
+                    <Avatar.Image size={80} source={{uri:'https://upload.wikimedia.org/wikipedia/commons/8/8c/Cristiano_Ronaldo_2018.jpg'}} />
+                    <Text variant="headlineSmall" style={styles.userName}>{userNameN}</Text>
                 </View>
-            </ScrollView>
-        </View>
+                <ScrollView style={styles.mainContent}>
+                    <Text variant="titleLarge" style={{color: Colors.primary, fontWeight: 'bold', marginBottom: 10}}>
+                        Mi información
+                    </Text>
+                    <Card style={styles.cardInfo} mode='elevated'>
+                        <Text variant="titleMedium" style={{color: Colors.primary, fontWeight: 'bold'}}>
+                            Correo electrónico
+                        </Text>
+                        <View style={styles.infoItem}>
+                            <MaterialCommunityIcons name="email" size={24} color={Colors.primary} style={{marginRight: 10}}/>
+                            <Text variant="bodyLarge">{userMail}</Text>
+                        </View>
+                        <Text variant="titleMedium" style={{color: Colors.primary, fontWeight: 'bold'}}>
+                            Número teléfonico
+                        </Text>
+                        <View style={styles.infoItem}>
+                            <MaterialCommunityIcons name="phone" size={24} color={Colors.primary} style={{marginRight: 10}}/>
+                            <Text variant="bodyLarge">{userCel}</Text>
+                        </View>
+                        <Text variant="titleMedium" style={{color: Colors.primary, fontWeight: 'bold'}}>
+                            DUI
+                        </Text>
+                        <View style={styles.infoItem}>
+                            <MaterialCommunityIcons name="clipboard-account" size={24} color={Colors.primary} style={{marginRight: 10}}/>
+                            <Text variant="bodyLarge">{userDui}</Text>
+                        </View>
+                    </Card>
+                    <View style={styles.options}>
+                        <TouchableWithoutFeedback onPress={() => {}}>
+                            <View style={styles.optionItem}>
+                                <MaterialCommunityIcons name="car" size={24} color={Colors.primary} style={{marginRight: 10, marginLeft: 5}}/>
+                                <Text variant="bodyLarge">Ver mis vehículos</Text>
+                            </View>
+                        </TouchableWithoutFeedback>
+                        <TouchableWithoutFeedback onPress={showModal}>
+                            <View style={styles.optionItem}>
+                                <MaterialCommunityIcons name="square-edit-outline" size={24} color={Colors.primary} style={{marginRight: 10, marginLeft: 5}}/>
+                                <Text variant="bodyLarge">Editar información</Text>
+                            </View>
+                        </TouchableWithoutFeedback>
+                        <TouchableWithoutFeedback onPress={Logout}>
+                            <View style={styles.optionItem}>
+                                <MaterialCommunityIcons name="logout" size={24} color={Colors.primary} style={{marginRight: 10, marginLeft: 5}}/>
+                                <Text variant="bodyLarge">Cerrar sesión</Text>
+                            </View>
+                        </TouchableWithoutFeedback>
+                    </View>
+                </ScrollView>
+            </View>
+        </Provider>
     );
 }
 
